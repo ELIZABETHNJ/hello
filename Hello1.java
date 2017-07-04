95d39294ebd331fbf27dfd22243c99f71585b2e1
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-04 14:39:19 +0530

    Update and rename Hello.java to Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,17 @@
+# hello
+import java.io.*;
+import java.util.*;
+public class Hello1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner (System.in);
+String ch;
+System.out.println("Enter the word");
+ch=sc.nextLine();
+for(int i=0;i<5;i++)
+{
+System.out.println(ch);
+}
+}
+}
